70e166f3fdfe874d225fb4131a8fcf878d14881e
Douglas Lacerda <dsldouglas@hotmail.com>
2021-09-17 13:12:24 -0300

    contatos: implement Search

--- contatos/src/screens/Search.jsx
+++ contatos/src/screens/Search.jsx
@@ -1,10 +1,27 @@
-import React, { Fragment } from 'react'
-import {Title} from '../components/ui/typos'
+import React, { Fragment, useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+import { InputText } from '../components/ui/form'
+import { Title } from '../components/ui/typos'
+import ShowPerson from './ShowPerson'
 
-export default function Search(props) {
+export default function Search() {
+    const people = useSelector(state => state)
+    const [search, setSearch] = useState('')
+    const [filteredPeople, setFilteredPeople] = useState([])
+    useEffect( () => {
+        const rs = people.filter(person => person.name.toLocaleLowerCase().includes(search.toLocaleLowerCase()))
+        setFilteredPeople(rs)
+    }, [search])
+
     return (
         <Fragment>
             <Title>Search</Title>
+            <InputText value={search} onChange={e => setSearch(e.target.value)}></InputText>
+            {
+                filteredPeople.map(person => (
+                    <ShowPerson {...person} />
+                ))
+            }
         </Fragment>
     )
 }
